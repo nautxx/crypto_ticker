bd016390388b87f95bbd7a5b1ed27477ca68de7a
naut <jrbarnachea@gmail.com>
2021-04-26 13:06:02 -0700

    Update cryptickr
    added api_key, time library to limit api requests, loop to adjust amount of times the information scrolls by.

--- cryptickr.py
+++ cryptickr.py
@@ -4,21 +4,30 @@ from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, ATARI_FONT, CP437_FONT, LCD_FONT, SEG7_FONT, SINCLAIR_FONT, SPECCY_FONT, TINY_FONT
+import time
 
-coin = "BTC"    #BTC, DOGE, ETH, etc.
-currency = "USD"    #USD, AUD, DOGE, ETH, etc.
+
+coin = "BTC"    #Cryptocoin Ticker Symbol: BTC, DOGE, ETH, etc.
+currency = "USD"    #Currency Abbreviation: USD, AUD, DOGE, ETH, etc.
+
+api_key = "8ec5a22972553ddb1eff5a049dd2947b43e0d00237c0ffaae719a6e9a2eef3b8"    #None if no api_key
+link = "https://min-api.cryptocompare.com/data/pricemulti?fsyms={0}&tsyms={1}".format(coin, currency)
+
+if api_key is not None:
+    link += "&api_key=" + api_key
 
 while True:
-
-    page = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms={0}&tsyms={1}".format(coin, currency))
+    page = requests.get(link)
     page_parsed = json.loads(page.text)
 
     price = page_parsed[coin][currency]
-    price_format = "{:,}".format(price)
+    price_format = "{:,}".format(price) #adds commas for readability
 
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=4, block_orientation=-90, rotate=2, contrast=1)
 
     message = coin.lower() + "-" + currency.lower() + ": $" + str(price_format)
 
-    show_message(device, message, y_offset=0, fill="white", font=proportional(TINY_FONT), scroll_delay=0.06)
+    for x in range(3):
+        show_message(device, message, y_offset=0, fill="white", font=proportional(TINY_FONT), scroll_delay=0.06)
+    time.sleep(300)   #adds time delay in (s, seconds) before looping
