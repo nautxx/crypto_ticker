4ccfebd36afac15e790bf3ea4bf65cd8291db6bd
naut <jrbarnachea@gmail.com>
2021-05-09 08:22:30 -0700

    Remove api key from config.py
    Updated to remove api key and start as default. User can add own api key if needed.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_db = {
-    "key":"8ec5a22972553ddb1eff5a049dd2947b43e0d00237c0ffaae719a6e9a2eef3b8",
+    "key":"",
     "link":"https://min-api.cryptocompare.com/data/pricemulti?fsyms={0}&tsyms={1}",
     "freq": 300
 }
