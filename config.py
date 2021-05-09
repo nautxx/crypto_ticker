55a42c5b2c487ba91a1761be88c82f366741521b
naut <jrbarnachea@gmail.com>
2021-05-08 21:26:17 -0700

    Add config.py
    Add config.py to store api information and adjust coin and currency values

---
+++ config.py
@@ -0,0 +1,10 @@
+api_db = {
+    "key":"8ec5a22972553ddb1eff5a049dd2947b43e0d00237c0ffaae719a6e9a2eef3b8",
+    "link":"https://min-api.cryptocompare.com/data/pricemulti?fsyms={0}&tsyms={1}",
+    "freq": 300
+}
+
+crypto_currency = {
+    "coin":"BTC,DOGE,ETH",
+    "currency":"USD"
+}
