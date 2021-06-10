b642288e14aee50a2043e9f1986dff3aff094b71
naut <jrbarnachea@gmail.com>
2021-06-09 18:31:16 -0700

    Update config.py
    currency values no longer needed and have been removed. user to determine from within cli. api key information removed. api_db renamed to config.

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
-api_db = {
-    "key":"",
+config = {
     "link":"https://min-api.cryptocompare.com/data/pricemulti?fsyms={0}&tsyms={1}",
-    "freq": 300
-}
-
-crypto_currency = {
-    "coin":"BTC,DOGE,ETH",
-    "currency":"USD"
+    "frequency": 300
 }
