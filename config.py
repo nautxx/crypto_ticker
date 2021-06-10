816d0ee555143ee808a027ec69d1daf2a23b912a
naut <jrbarnachea@gmail.com>
2021-06-09 22:02:19 -0700

    Update config.py
    Temporary link2 added to replace link once fully migrated from get_price function.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 config = {
     "link":"https://min-api.cryptocompare.com/data/pricemulti?fsyms={0}&tsyms={1}",
+    "link2":"https://min-api.cryptocompare.com/data/pricemultifull?fsyms={0}&tsyms={1}",
     "frequency": 300
 }
