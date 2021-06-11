0ee537a101a6b8c9419d7bb4832535a1bbd858b8
naut <jrbarnachea@gmail.com>
2021-06-10 18:19:40 -0700

    Update crypto_ticker.py
    More variables stored inside get_data function. New information and columns added to terminal output. Some comments modified.

--- crypto_ticker.py
+++ crypto_ticker.py
@@ -11,7 +11,8 @@ if system:  #skip luma load when using mac or windows operating systems
     from luma.led_matrix.device import max7219
     from luma.core.interface.serial import spi, noop
     from luma.core.legacy import text, show_message
-    from luma.core.legacy.font import proportional, ATARI_FONT, CP437_FONT, LCD_FONT, SEG7_FONT, SINCLAIR_FONT, SPECCY_FONT, TINY_FONT
+    from luma.core.legacy.font import proportional, ATARI_FONT, CP437_FONT, \
+        LCD_FONT, SEG7_FONT, SINCLAIR_FONT, SPECCY_FONT, TINY_FONT
 
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=4, block_orientation=-90, rotate=2, contrast=1)
@@ -71,38 +72,65 @@ def get_data(link, spacing=1):
             if coin != currency:
                 for category in link['RAW'][coin][currency]:
                     value = link['RAW'][coin][currency][category]   
-                    currencysymbol = link['RAW'][coin][currency]['TOSYMBOL']
+                    currency_symbol = link['RAW'][coin][currency]['TOSYMBOL']
                     price = link['RAW'][coin][currency]['PRICE']
-                    twentyfourhour = link['RAW'][coin][currency]['CHANGE24HOUR']
-                    twentyfourhour_pct = link['RAW'][coin][currency]['CHANGEPCT24HOUR']
+                    last_update = (datetime.fromtimestamp(link['RAW'][coin][currency]['LASTUPDATE']) - timedelta(hours=0)).strftime('%Y-%m-%d %H:%M:%S')
+                    twenty_four_hour_low = link['RAW'][coin][currency]['LOW24HOUR']
+                    twenty_four_hour_high = link['RAW'][coin][currency]['HIGH24HOUR']
+                    twenty_four_hour = link['RAW'][coin][currency]['CHANGE24HOUR']
+                    twenty_four_hour_pct = link['RAW'][coin][currency]['CHANGEPCT24HOUR']
+                    hour = link['RAW'][coin][currency]['CHANGEHOUR']
+                    hour_pct = link['RAW'][coin][currency]['CHANGEPCTHOUR']
+                    supply = link['RAW'][coin][currency]['SUPPLY']
+
                     if price <= 1 and price >= -1:
                         price_short = '{0:,.8f}'.format(float(price))
                     else:
                         price_short = '{0:,.2f}'.format(float(price))
-                    if twentyfourhour <= 1 and twentyfourhour >=-1:
-                        twentyfourhour_short = '{0:,.8f}'.format(float(twentyfourhour))
-                    else:
-                        twentyfourhour_short = '{0:,.2f}'.format(float(twentyfourhour))
-                    if twentyfourhour_pct <= 1 and twentyfourhour_pct >=-1:
-                        twentyfourhour_pct_short = '{0:,.8f}'.format(float(twentyfourhour_pct))
-                    else:
-                        twentyfourhour_pct_short = '{0:,.3f}'.format(float(twentyfourhour_pct))
-                    twentyfourhour_pct_num = int(twentyfourhour_pct)
-                    if twentyfourhour_pct_num > 0:
-                        twentyfourhour_symbol = '\x1b[38;5;12m'
-                        twentyfourhour_color = '\x1b[38;5;12m'
-                    else:
-                        twentyfourhour_symbol = '\x1b[38;5;1m'
-                        twentyfourhour_color = '\x1b[38;5;1m'
-                    hour = link['RAW'][coin][currency]['CHANGEHOUR']
-                    hour_pct = link['RAW'][coin][currency]['CHANGEPCTHOUR']
-                    twentyfourhour_low = link['RAW'][coin][currency]['LOW24HOUR']
-                    twentyfourhour_high = link['RAW'][coin][currency]['HIGH24HOUR']
 
-            output += '\x1b[0;5;37m' + str(coin) + '-' + currencysymbol.ljust(5,' ') + '\t' \
-                    + str(price_short).ljust(14,' ') + twentyfourhour_color \
-                    + str(twentyfourhour_short).ljust(14,' ') + twentyfourhour_symbol \
-                    + '(' + str(twentyfourhour_pct_short) + '%)\n' + '\x1b[0m'    
+                    if twenty_four_hour <= 1 and twenty_four_hour >=-1:
+                        twenty_four_hour_short = '{0:+,.8f}'.format(float(twenty_four_hour))
+                    else:
+                        twenty_four_hour_short = '{0:+,.2f}'.format(float(twenty_four_hour))
+
+                    if twenty_four_hour_pct <= 1 and twenty_four_hour_pct >=-1:
+                        twenty_four_hour_pct_short = '{0:+,.2f}'.format(float(twenty_four_hour_pct))
+                    else:
+                        twenty_four_hour_pct_short = '{0:+,.2f}'.format(float(twenty_four_hour_pct))
+
+                    twenty_four_hour_pct_num = int(twenty_four_hour_pct)
+                    if twenty_four_hour_pct_num >= 0:
+                        twenty_four_hour_symbol = '\x1b[38;5;12m'
+                        twenty_four_hour_color = '\x1b[38;5;12m'
+                    else:
+                        twenty_four_hour_symbol = '\x1b[38;5;1m'
+                        twenty_four_hour_color = '\x1b[38;5;1m'
+
+                    if hour <= 1 and hour >=-1:
+                        hour_short = '{0:+,.8f}'.format(float(hour))
+                    else:
+                        hour_short = '{0:+,.2f}'.format(float(hour))
+
+                    if hour_pct <= 1 and hour_pct >=-1:
+                        hour_pct_short = '{0:+,.2f}'.format(float(hour_pct))
+                    else:
+                        hour_pct_short = '{0:+,.2f}'.format(float(hour_pct))
+                        
+                    hour_pct_num = int(hour_pct)
+                    if hour_pct_num >= 0:
+                        hour_symbol = '\x1b[38;5;12m'
+                        hour_color = '\x1b[38;5;12m'
+                    else:
+                        hour_symbol = '\x1b[38;5;1m'
+                        hour_color = '\x1b[38;5;1m'
+
+            output += '\x1b[0;5;37m' + str(coin + '-' + currency_symbol).ljust(12,' ') \
+                    + str(price_short).rjust(12,' ') + '\t' + twenty_four_hour_color \
+                    + str(twenty_four_hour_short).rjust(12,' ') + '\t' + twenty_four_hour_symbol \
+                    + str('(' + twenty_four_hour_pct_short + '%)').rjust(10,' ') \
+                    + str(hour_short).rjust(12,' ') + '\t' + hour_symbol \
+                    + str('(' + hour_pct_short + '%)').rjust(10,' ') + '\x1b[0m' \
+                    + str(last_update + '\n').rjust(24,' ')
     return output
 
 def ticker_display(set_range=1):
@@ -116,11 +144,12 @@ def cryptoticker_endless(ctx):  # loop to infiniti
         parsed_link = parse_the_link()
 #            terminal_message, ticker_message = get_prices(parsed_link)
         terminal_message = get_data(parsed_link)
-        print('\n' + "           \tPrice         24hr          pct" + '\n' + terminal_message)
+        print('\n' + "           \t   Price                24hr           pct         1hr         pct            Last update" + '\n' + terminal_message)
         print("Next Update: ".lower(), get_next_timestamp())
+        print("Press 'Ctrl + C' to exit")
         if system:
             ticker_display()
-        time.sleep(config['frequency'])   # adds time delay in (s, seconds) before looping
+        time.sleep(config['frequency'])   # loop time delay in (s, seconds)
 
 @main.command()
 @click.pass_obj
