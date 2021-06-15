aeda28df6438df5cbe9ee9abdd89ad3620d4e970
naut <jrbarnachea@gmail.com>
2021-06-14 17:48:37 -0700

    Update crypto_ticker.py
    User not longer prompted for a message and displaying message fixed when using cryptoticker_endless function. print mssages added to messagebar functions let user know their message was sent. Added function type to message log. Some comment positions moved to satisfy PEP. Ability to store api_key in config.py restored. User can enter in cli or store in config.py

--- crypto_ticker.py
+++ crypto_ticker.py
@@ -8,8 +8,9 @@ from config import config
 from pathlib import Path
 from color import Color
 
+# skips luma libraries from loading when using mac or windows operating systems
 system = platform.system() not in ['Darwin', 'Java', 'Windows']
-if system:  #skip luma load when using mac or windows operating systems
+if system:  
     from luma.core.interface.serial import spi, noop
     from luma.core.render import canvas
     from luma.core.legacy import text, show_message
@@ -31,29 +32,35 @@ class User(object): # initialize user data
 @click.option('--coin', default='BTC', help='Crypto ticker symbol. Multiple ticker symbols separate with comma')
 @click.option('--currency', default='USD', help='Currency. Multiple currencies separate with comma')
 @click.option('--apikey', default=None, help='Enter API key from cryptocompare.com')
-@click.option('--message', prompt='Your message', help='Enter message to display for message_bar')
+@click.option('--message', default=None, help='Enter message to display for message_bar')
 @click.option('--count', default=1, help='Scrolling: Number of times to loop display. Static: Time in seconds to display')
 @click.pass_context
-def main(ctx, coin, currency, apikey, message, count):  # user stored values entered in cli
+# user stored values entered in cli
+def main(ctx, coin, currency, apikey, message, count):
     ctx.obj = User(coin, currency, apikey, message, count)
 
 @click.pass_obj
 def parse_the_link(ctx):
     link = config['link'].format(ctx.coin, ctx.currency)
+    # checks if api key is entered in cli or saved in config.py
     if ctx.apikey is not None:
         link += "&api_key=" + ctx.apikey
+    elif config['api_key'] != "":
+        link += "&api_key=" + config['api_key']
+
     page = requests.get(link)
     page_parsed = json.loads(page.text)
     return page_parsed
 
 def get_current_timestamp():
-    return datetime.now().strftime("%Y.%m.%d %H:%M:%S")
-
-def get_next_timestamp():
-    timestamp = (datetime.now() + timedelta(seconds=config['frequency'])).strftime("%Y.%m.%d %H:%M")
+    timestamp = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
     return timestamp
 
-def get_data(link, spacing=1):
+def get_next_timestamp():
+    timestamp = (datetime.now() + timedelta(seconds=config['frequency'])).strftime("%Y.%m.%d %H:%M:%S")
+    return timestamp
+
+def get_data(link):
     output, output_display = '', ''
     for coin in link['RAW']:
         for currency in link['RAW'][coin]:
@@ -119,10 +126,10 @@ def get_data(link, spacing=1):
 
     return output, output_display
 
-def logger(message):
+def logger(message, function):
     path = Path(__file__).parent.absolute()
     file = open(str(path) + "/message_log.txt", "a")
-    file.write("\n" + get_current_timestamp() + ": " + message)
+    file.write("\n" + get_current_timestamp() + "_" + function + ": " + message)
     file.close
 
 @main.command()
@@ -136,14 +143,15 @@ def cryptoticker_endless(ctx):  # loop to infiniti
         print("Press 'Ctrl + C' to exit")
         if system:
             for tick in range(ctx.count):
-                show_message(device, message, y_offset=0, fill='white', font=proportional(TINY_FONT), scroll_delay=0.06)
+                show_message(device, ticker_message, y_offset=0, fill='white', font=proportional(TINY_FONT), scroll_delay=0.06)
         time.sleep(config['frequency'])   # loop time delay in (s, seconds)
 
 @main.command()
 @click.pass_obj
 def messagebar_scrolling(ctx):
-    logger(ctx.message)
-    print('Message "' + ctx.message + '" sent successfully.')
+    if ctx.message is None:
+        ctx. message = input("Your message: ")
+    logger(ctx.message, "scrolling")
 
     if system:
         label = 'Displaying'
@@ -151,17 +159,21 @@ def messagebar_scrolling(ctx):
         empty_char = click.style('-', fg='white', dim=True)
         length = 100
 
+        print('Message "' + ctx.message + '" sent successfully.')
         with click.progressbar(label=label, length=length, fill_char=fill_char, empty_char=empty_char, show_eta=False) as bar:
             bar.update(0)
             for letter in range(ctx.count):
                 show_message(device, ctx.message, fill='white', font=proportional(TINY_FONT), scroll_delay=0.06)
                 bar.update(length/ctx.count)
+    else:
+        print('Message "' + ctx.message + '" has been logged successfully.')        
 
 @main.command()
 @click.pass_obj
 def messagebar_static(ctx):
-    logger(ctx.message)
-    print('Message "' + ctx.message + '" sent successfully.')
+    if ctx.message is None:
+        ctx. message = input("Your message: ")
+    logger(ctx.message, "static")
 
     if system:
         with canvas(device) as draw:
