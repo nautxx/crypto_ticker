10efca2d47c320d7953790c8cc61f3c52ac88346
naut <jrbarnachea@gmail.com>
2022-03-22 14:24:19 -0700

    Renamed crypto_data.py to ticker_data.py

--- crypto_data.py
+++
@@ -1,4 +0,0 @@
-# store data pulled from api here.
-class CryptoData:
-    def __init__(self):
-        pass
---
+++ ticker_data.py
@@ -0,0 +1,7 @@
+# store data pulled from api here.
+class TickerData:
+    """
+    Retains data obtained from get_crypto_data
+    """
+    def __init__(self):
+        pass
