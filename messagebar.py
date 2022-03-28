faf001e5bb103257ffbf20de014b6e9c5c4154ca
naut <jrbarnachea@gmail.com>
2022-03-28 10:58:01 -0700

    Update messagebar.py

--- messagebar.py
+++ messagebar.py
@@ -2,3 +2,9 @@ class MessageBar:
 
     def __init__(self):
         pass
+
+    def logger(self):
+        path = Path(__file__).parent.absolute()
+        file = open(str(path) + "/message_log.txt", "a")
+        file.write("\n" + get_current_timestamp() + "_" + function + ": " + message)
+        file.close
