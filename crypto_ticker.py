c0a81d0c3ea03dd7560608c15deb76621af238f7
naut <jrbarnachea@gmail.com>
2022-06-07 14:51:09 -0700

    Update crypto_ticker.py

--- crypto_ticker.py
+++ crypto_ticker.py
@@ -38,6 +38,8 @@ def get_next_update_timestamp():
     next_time_stamp = (datetime.now() + timedelta(seconds=DELAY))
     next_time_stamp_formatted = next_time_stamp.strftime("%Y.%m.%d %H:%M:%S")
     return next_time_stamp
+
+    
 class User(object): 
     """Initializes user data input from command line."""
     def __init__(self, coin=None, currency=None, apikey=None, message=None, count=1):
