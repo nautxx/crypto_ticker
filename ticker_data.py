983450489503d918b2fc6fb11000a70a7360c3a2
naut <jrbarnachea@gmail.com>
2022-06-07 14:58:11 -0700

    Update ticker_data.py

--- ticker_data.py
+++ ticker_data.py
@@ -2,7 +2,12 @@ class TickerData:
     """
     Retains data obtained from get_crypto_data
     """
-    def __init__(self, currency_symbol, price, last_update, twenty_four_hour_low, twenty_four_hour_high, twenty_four_hour_change, twenty_four_hour_change_pct, hour_change, hour_change_pct, supply):
+    def __init__(
+        self, currency_symbol, price, last_update, twenty_four_hour_low, 
+        twenty_four_hour_high, twenty_four_hour_change, 
+        twenty_four_hour_change_pct, hour_change, hour_change_pct, supply
+        ):
+        
         self.currency_symbol = currency_symbol
         self.price = price
         self.last_update = last_update
